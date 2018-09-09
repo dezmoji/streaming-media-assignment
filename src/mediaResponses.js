3b4963c594cd1651b23def844f2fa9fbe91fe839
Dezmon Gilbert <dog6487@rit.edu>
2018-09-09 16:25:39 -0400

    Removed package-lock file

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -2,64 +2,64 @@ const fs = require('fs');
 const path = require('path');
 
 const getStream = (request, response, filename, contentType) => {
-    const file = path.resolve(__dirname, filename);
+  const file = path.resolve(__dirname, filename);
 
-    fs.stat(file, (err, stats) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                response.writeHead(404);
-            }
-            return response.end(err);
-        }
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        response.writeHead(404);
+      }
+      return response.end(err);
+    }
 
-        let { range } = request.headers;
+    let { range } = request.headers;
 
-        if (!range) {
-            range = 'bytes=0-';
-        }
+    if (!range) {
+      range = 'bytes=0-';
+    }
 
-        const positions = range.replace(/bytes=/, '').split('-');
+    const positions = range.replace(/bytes=/, '').split('-');
 
-        let start = parseInt(positions[0], 10);
+    let start = parseInt(positions[0], 10);
 
-        const total = stats.size;
-        const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    const total = stats.size;
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
 
-        if (start > end) {
-            start = end - 1;
-        }
+    if (start > end) {
+      start = end - 1;
+    }
 
-        const chunksize = (end - start) + 1;
+    const chunksize = (end - start) + 1;
 
-        response.writeHead(206, {
-            'Content-Range': `bytes ${start}-${end}/${total}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': contentType,
-        });
-
-        const stream = fs.createReadStream(file, { start, end });
-
-        stream.on('open', () => {
-            stream.pipe(response);
-        });
-
-        stream.on('error', (streamErr) => {
-            response.end(streamErr);
-        });
-
-        return stream;
+    response.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': contentType,
     });
+
+    const stream = fs.createReadStream(file, { start, end });
+
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      response.end(streamErr);
+    });
+
+    return stream;
+  });
 };
 
 const getParty = (request, response) => {
-    getStream(request, response, '../client/party.mp4', 'video/mp4');
+  getStream(request, response, '../client/party.mp4', 'video/mp4');
 };
 const getBling = (request, response) => {
-    getStream(request, response, '../client/bling.mp3', 'audio/mpeg');
+  getStream(request, response, '../client/bling.mp3', 'audio/mpeg');
 };
 const getBird = (request, response) => {
-    getStream(request, response, '../client/bird.mp4', 'video/mp4');
+  getStream(request, response, '../client/bird.mp4', 'video/mp4');
 };
 
 module.exports.getParty = getParty;
